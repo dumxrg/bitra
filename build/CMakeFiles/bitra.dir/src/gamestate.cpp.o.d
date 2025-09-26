9168760040ef52e6163530ebda0b6902c84f3c33
Morgan <154711533+dumxrg@users.noreply.github.com>
2025-09-26 17:38:03 +0000

    Delete build directory

--- build/CMakeFiles/bitra.dir/src/gamestate.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/bitra.dir/src/gamestate.cpp.o: \
- /home/dumxrg/bitra/src/gamestate.cpp /usr/include/stdc-predef.h \
- /home/dumxrg/bitra/src/../include/gamestate.h /usr/include/raylib.h \
- /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/include/stdarg.h
--- build/CMakeFiles/bitra.dir/src/menu.cpp.o.d
+++
@@ -1,6 +0,0 @@
-CMakeFiles/bitra.dir/src/menu.cpp.o: /home/dumxrg/bitra/src/menu.cpp \
- /usr/include/stdc-predef.h /home/dumxrg/bitra/src/../include/menu.h \
- /usr/include/raylib.h \
- /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/include/stdarg.h \
- /home/dumxrg/bitra/src/../include/gamestate.h \
- /home/dumxrg/bitra/src/../include/window.h
